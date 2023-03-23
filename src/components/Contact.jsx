758d1b2cbe998904448cf3ff2107042c2484771c
VickyIr <vicky.ir@students.amikom.ac.id>
2023-03-23 22:44:39 +0700

    portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+gimport React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
